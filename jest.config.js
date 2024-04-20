255b02cd69040cc49a17c51fdcd3fa9074ac74fe
Oleg <olegz11m@gmail.com>
2024-04-19 21:10:57 -0400

    create new advance file

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  "reporters": [
+     "default", ["jest-junit", {outputDirectory: "reports"}], ["jest-html-reporters", {publicPath: "reports" }]
+    ],
 };
